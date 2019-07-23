2b16a569a616dc69d0f870b4f42c935054c5c0f9
tumpji <tumpji@gmail.com>
2019-07-23 12:10:17 +0200

    Added init function

--- snake/shared.cpp
+++ snake/shared.cpp
@@ -1,9 +1,20 @@
+#include <boost/python/numpy.hpp>
+
 #include "snake.h"
 
+namespace p = boost::python;
+namespace np = boost::python::numpy;
 
+
+int init_lib () {
+    Py_Initialize();
+    np::initialize();
+}
 
 int test () {
     Snake<DEF_BOARD_SIZE_X,DEF_BOARD_SIZE_Y,1> h;
 
     return 1;
 }
+
+
