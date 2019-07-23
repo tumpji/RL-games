59673dcf5d2af16607debf7b7a37e76040e8cda1
tumpji <tumpji@gmail.com>
2019-07-23 20:12:06 +0200

    Added test if numpy library is linked

--- snake/shared.cpp
+++ snake/shared.cpp
@@ -1,11 +1,10 @@
 #include <boost/python.hpp>
-
-//#include <boost/python/numpy.hpp>
-//namespace np = boost::python::numpy;
+#include <boost/python/numpy.hpp>
 
 #include "snake.h"
 
 namespace p = boost::python;
+namespace np = boost::python::numpy;
 
 /*
 int main(int argc, char **argv) {
@@ -17,40 +16,32 @@ int main(int argc, char **argv) {
 
 char const* test () {
     Snake<DEF_BOARD_SIZE_X,DEF_BOARD_SIZE_Y,1> h;
+
     return "hellp wordsf";
 }
 
-BOOST_PYTHON_MODULE(NAME)
-{
-    using namespace boost::python;
-    def("test", test);
-}
-
-
-
-
-
-
-
-
-
-//def(PYINIT_NAME_S, PYINIT_NAME);
-//def("create_new_games", create_new_games);
-
-/*
-*/
-
-//PyMODINIT_FUNC PYINIT_NAME () { }
-/*
 np::ndarray create_new_games(int ngames) {
     // for now it returns only zeros ngames x 10 x 10
     np::dtype dt = np::dtype::get_builtin<int>();
     np::ndarray out = np::zeros(p::make_tuple(ngames, 10, 10), dt);
     return out;
 }
-*/
-/*
-int test () {
-    return 1;
+
+BOOST_PYTHON_MODULE(NAME)
+{
+    using namespace boost::python;
+
+    np::initialize();
+
+    def("test", test);
+    def("create_new_games", create_new_games);
 }
-*/
+
+
+
+
+
+
+
+
+
